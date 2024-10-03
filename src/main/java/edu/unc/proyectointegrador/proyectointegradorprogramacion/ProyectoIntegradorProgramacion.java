559c790b7de655891c192dca1f93813ab82acfa0
agustin <agus_blackarrow_00@hotmail.com>
2024-10-03 14:56:17 -0700

    Primer commit

---
+++ src/main/java/edu/unc/proyectointegrador/proyectointegradorprogramacion/ProyectoIntegradorProgramacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.unc.proyectointegrador.proyectointegradorprogramacion;
+
+/**
+ *
+ * @author estudiante
+ */
+public class ProyectoIntegradorProgramacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

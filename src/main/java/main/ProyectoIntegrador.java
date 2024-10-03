2599ee564984d52273b83f0bc15605b515220781
agustin <agus_blackarrow_00@hotmail.com>
2024-10-03 15:59:04 -0700

    Arreglando pedos.

--- src/main/java/main/ProyectoIntegrador.java
+++ src/main/java/main/ProyectoIntegrador.java
@@ -8,7 +8,7 @@ package main;
  *
  * @author estudiante
  */
-public class ProyectoIntegradorProgramacion {
+public class ProyectoIntegrador {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
